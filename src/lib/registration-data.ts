ec8fb05750c3233d3f9c0d1ba45e25a021b0d66a
Hamza Abdul Rauf <hhharc123@gmail.com>
2026-08-16 16:39:56 +0500

    done2

--- src/lib/registration-data.ts
+++ src/lib/registration-data.ts
@@ -4,7 +4,10 @@ export const PREFERRED_SLEEVE_OPTIONS = ["Full Sleeves", "Half Sleeves"];
 
 export const AVAILABILITY_OPTIONS = ["Available all matches", "Missing few matches"];
 
-export const NOT_AVAILABLE_ON_OPTIONS = ["09 Oct 2026", "10 Oct 2026", "11 Oct 2026"];
+export const NOT_AVAILABLE_ON_OPTIONS = [
+  "11 Sep 2026 — 19:00 onwards",
+  "13 Sep 2026 — 17:00 onwards",
+];
 
 export const MAX_FILE_SIZE = 2 * 1024 * 1024;
 export const ALLOWED_FILE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
